8e867ec5cf6e0a5471d08e21fe1aa295c8e8fc76
zakariamohamed123 <zakariamohamed2022@gmail.com>
2024-07-08 16:21:09 +0300

    done

--- server/models.py
+++ server/models.py
@@ -12,3 +12,20 @@ class Message(db.Model, SerializerMixin):
     __tablename__ = 'messages'
 
     id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String)
+    username = db.Column(db.String)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'body': self.body,
+            'username': self.username,
+            'created_at': self.created_at,
+            'updated_at': self.updated_at
+        }
+    
+    def __repr__(self):
+        return f'<Message {self.id}, {self.body}, {self.username}, {self.created_at}, {self.updated_at}>'
+    
